022ed4962630311e9d45f1ad790b18fe6a018206
Andrew <sadunenko88@gmail.com>
2016-11-10 11:56:59 +0200

    add additional submit event

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-document.forms["mainForm"].onsubmit = function() {
-	greet(document.getElementById("greetText"), document.getElementById("username"));
-};
-
 function greet(p, name) {
 	p.innerHTML = "Hello, " + name.value + "!";
 };
+
+function writeHHMM(p) {
+	var n = new Date();
+	var hh = "HH: " + n.getHours();
+	var mm = " MM: " + n.getMinutes();
+
+	p.innerHTML = hh + mm;
+};
+
+var myForm = document.getElementById("mainForm");
+myForm.addEventListener("submit", function(){greet(document.getElementById("greetText"), document.getElementById("username"))}, false);
+myForm.addEventListener("submit", function(){writeHHMM(document.getElementById("HHMMText"))}, false);
