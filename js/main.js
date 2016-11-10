7f11cfa3883323272a71b9887a8153f5a791ff08
Andrew <sadunenko88@gmail.com>
2016-11-10 10:12:39 +0200

    move onSubmit handler to js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+document.getElementById("mainForm").onsubmit = function() {
+	greet(document.getElementById("greetText"), document.getElementById("username"));
+};
+
 function greet(p, name) {
-		p.innerHTML = "Hello, " + name.value + "!";
-	};
+	p.innerHTML = "Hello, " + name.value + "!";
+};
