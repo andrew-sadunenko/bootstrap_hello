e98a1b4d14e49d9b260c6f526c12f77a32009364
Andrew <sadunenko88@gmail.com>
2016-11-12 18:27:05 +0200

    removeEventListener

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
 function greet(p, name) {
+	console.log("greet");
+
+	if (!document.getElementById("dtCheckBox").checked) {
+		removeOnSubmitEvents();	
+	};
+
 	p.innerHTML = "Hello, " + name.value + "!";
 };
 
 function writeHHMM(p) {
+	console.log("HHMM");
 	var n = new Date();
-	var hh = "HH: " + n.getHours();
-	var mm = " MM: " + n.getMinutes();
+	var hh = n.getHours();
+	var mm = n.getMinutes();
 
-	p.innerHTML = hh + mm;
+	p.innerHTML = hh + ":" + mm;
 };
 
+function writeYYYY(p) {
+	console.log("YYYY");
+	var n = new Date();
+	var yyyy = n.getFullYear();
+
+	p.innerHTML = yyyy;
+};
+
+var mySubmitEvents = new Array();
 var myForm = document.getElementById("mainForm");
-myForm.addEventListener("submit", function(){greet(document.getElementById("greetText"), document.getElementById("username"))}, false);
-myForm.addEventListener("submit", function(){writeHHMM(document.getElementById("HHMMText"))}, false);
+
+myForm.addEventListener("submit", mySubmitEvents[mySubmitEvents.length] = function() {
+	greet(document.getElementById("greetText"), document.getElementById("username"))
+});
+
+myForm.addEventListener("submit", mySubmitEvents[mySubmitEvents.length] = function() {
+	writeHHMM(document.getElementById("HHMMText"))
+});
+
+myForm.addEventListener("submit", mySubmitEvents[mySubmitEvents.length] = function() {
+	writeYYYY(document.getElementById("YYYYText"))
+});
+
+function removeOnSubmitEvents() {
+	mySubmitEvents.forEach(function(element, index, mySubmitEvents) { 
+		myForm.removeEventListener("submit", element)
+	});
+};
